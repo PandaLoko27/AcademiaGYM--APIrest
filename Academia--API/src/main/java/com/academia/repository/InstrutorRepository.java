8ad149385a1d1f9ad6d0c472bca0fa623a5a8b6d
Otavio2007 <otavioaredes62@gmail.com>
2025-07-26 16:52:01 -0300

    Create InstrutorRepository.java

---
+++ Academia--API/src/main/java/com/academia/repository/InstrutorRepository.java
@@ -0,0 +1,6 @@
+package com.academia.repository;
+
+import com.academia.model.Instrutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {}
