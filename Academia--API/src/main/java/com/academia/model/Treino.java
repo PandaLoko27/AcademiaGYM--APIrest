1e52dd30e8d13e44a2c8b3b5ebc6a52a20f314f2
Otavio2007 <otavioaredes62@gmail.com>
2025-07-29 12:54:59 -0300

    Update Treino.java

--- Academia--API/src/main/java/com/academia/model/Treino.java
+++ Academia--API/src/main/java/com/academia/model/Treino.java
@@ -1,46 +1,38 @@
 package com.academia.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
 
 @Entity
 public class Treino {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Descrição é obrigatória")
     private String descricao;
 
     @ManyToOne
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
     @ManyToOne
+    @JoinColumn(name = "instrutor_id")
     private Instrutor instrutor;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public Aluno getAluno() {
-        return aluno;
-    }
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-    public Instrutor getInstrutor() {
-        return instrutor;
-    }
-    public void setInstrutor(Instrutor instrutor) {
-        this.instrutor = instrutor;
-    }
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public Aluno getAluno() { return aluno; }
+    public void setAluno(Aluno aluno) { this.aluno = aluno; }
+    public Instrutor getInstrutor() { return instrutor; }
+    public void setInstrutor(Instrutor instrutor) { this.instrutor = instrutor; }
+    public LocalDateTime getDataCriacao() { return dataCriacao; }
+    public void setDataCriacao(LocalDateTime dataCriacao) { this.dataCriacao = dataCriacao; }
 }
