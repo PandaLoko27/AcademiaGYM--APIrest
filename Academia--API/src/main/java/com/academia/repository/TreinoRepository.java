5e0b10923ebc6b5144af2227811cf3c5303f6892
Otavio2007 <otavioaredes62@gmail.com>
2025-07-29 11:44:57 -0300

    Update TreinoRepository.java

--- Academia--API/src/main/java/com/academia/repository/TreinoRepository.java
+++ Academia--API/src/main/java/com/academia/repository/TreinoRepository.java
@@ -1,6 +1,8 @@
 package com.academia.repository;
 
-import com.academia.model.Treino;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TreinoRepository extends JpaRepository<Treino, Long> {}
+import com.academia.model.Treino;
+
+public interface TreinoRepository extends JpaRepository<Treino, Long> {
+}
