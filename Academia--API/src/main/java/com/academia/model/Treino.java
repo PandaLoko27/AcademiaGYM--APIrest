9957fc808728282a4920660b2d4926254f659424
Otavio2007 <otavioaredes62@gmail.com>
2025-07-29 11:38:30 -0300

    Update Treino.java

--- Academia--API/src/main/java/com/academia/model/Treino.java
+++ Academia--API/src/main/java/com/academia/model/Treino.java
@@ -1,6 +1,10 @@
 package com.academia.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Treino {
@@ -15,5 +19,28 @@ public class Treino {
     @ManyToOne
     private Instrutor instrutor;
 
-    // getters e setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Aluno getAluno() {
+        return aluno;
+    }
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    public Instrutor getInstrutor() {
+        return instrutor;
+    }
+    public void setInstrutor(Instrutor instrutor) {
+        this.instrutor = instrutor;
+    }
 }
