9ca32c6dc1c46d4eb6688255c928f9487081238c
Otavio2007 <otavioaredes62@gmail.com>
2025-07-29 11:38:03 -0300

    Update Plano.java

--- Academia--API/src/main/java/com/academia/model/Plano.java
+++ Academia--API/src/main/java/com/academia/model/Plano.java
@@ -16,4 +16,27 @@ public class Plano {
     private BigDecimal valorMensal;
 
     // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValorMensal() {
+        return valorMensal;
+    }
+
+    public void setValorMensal(BigDecimal valorMensal) {
+        this.valorMensal = valorMensal;
+    }
 }
