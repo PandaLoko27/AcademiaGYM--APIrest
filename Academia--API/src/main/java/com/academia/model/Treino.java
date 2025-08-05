02a92d090147b76398ddf5e9083bd49011d32341
Otavio2007 <otavioaredes62@gmail.com>
2025-08-05 19:06:55 -0300

    Update Treino.java

--- Academia--API/src/main/java/com/academia/model/Treino.java
+++ Academia--API/src/main/java/com/academia/model/Treino.java
@@ -1,9 +1,16 @@
 package com.academia.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Treino {
